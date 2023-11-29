b6a9d8af665396da3f5e614c45609d9c466bb7dc
Sen Cao <hicaosenq@gmail.com>
2023-11-29 12:21:03 +0800

    添加文档title

--- source/conf.py
+++ source/conf.py
@@ -55,12 +55,8 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'furo'
-
+html_title = "足迹"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-html_theme_options = {
-    "sidebar_hide_name": True,
-}
